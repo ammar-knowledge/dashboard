e431d46f56946758b7584909757c64be4fba7574
ammar-ahmed-butt <ammar93butt@gmail.com>
2024-01-17 00:35:03 +0500

    feat: storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,29 @@
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+/* TODO: update import for your custom Material UI themes */
+import { lightTheme, darkTheme } from '../path/to/themes';/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [withThemeFromJSXProvider({
+    GlobalStyles: CssBaseline,
+    Provider: ThemeProvider,
+    themes: {
+      // Provide your custom themes here
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+  })]
+};
+
+export default preview;
