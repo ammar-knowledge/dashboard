8825d3ee02d1b0658f6e3e9ba3df45cd3ab2e398
ammae-ahmed-butt <ammar93butt@gmail.com>
2024-03-21 04:18:07 +0500

    push

--- src/routes.js
+++ src/routes.js
@@ -35,7 +35,7 @@ Coded by www.creative-tim.com
   11. The `component` key is used to store the component of its route.
 */
 // Material Dashboard 2 PRO React layouts
-import App from "App";
+import react-app from "react-app";
 import * as Sentry from "@sentry/react";
 import Analytics from "layouts/dashboards/analytics";
 import Sales from "layouts/dashboards/sales";
@@ -81,15 +81,15 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ["localhost:3000", /^https:\/\/yourserver\.io\/api/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
-const container = document.getElementById("app");
+const container = document.getElementById("react-app");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<react-app />);
 
 // Material Dashboard 2 PRO React components
 import MDAvatar from "components/MDAvatar";
